0ab456bd4e8a76dd5318685017b56401902b5068
micton-code <antho_ul@hotmail.com>
2025-12-18 17:41:15 -0400

    clase de reduce

---
+++ server/05-reduce.js
@@ -0,0 +1,15 @@
+const numeros = [1, 2, 3, 4];
+
+let sum = 0;
+// Usando for
+for (let index = 0; index < numeros.length; index++) {
+    const elemento = numeros[index];
+    sum += elemento; // sum = sum + elemento
+}
+console.log('Suma con for:', sum);
+
+// Usando reduce
+const sumaReduce = numeros.reduce((sum, elemento) => sum + elemento, 0);
+// el primer argumento es la función de reducción(arrowfunction), El segundo argumento (0) es el valor inicial de sum
+
+console.log('Suma con reduce:', sumaReduce);
